7ee704c2fcd8efb56c1450bffe66097227ab867c
Osank31 <osankverma2004@gmail.com>
2026-01-30 19:02:08 +0530

    feat: implement video editing functionality with audio management and clip array support

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { SocketContextProvider } from "@/context/SocketContext";
 import SocketProvider from "@/provider/SocketProvider";
 
 const geistSans = Geist({
