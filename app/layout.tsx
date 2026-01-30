a9a7a32e1440c1c20f4357689327ca2e036cf277
Zaki <123zakimd@gmail.com>
2026-01-30 19:05:02 +0530

    fix: correct import path for SocketProvider in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { SocketContextProvider } from "@/context/SocketContext";
-import SocketProvider from "@/provier/SocketProvider";
+import SocketProvider from "@/provider/SocketProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
